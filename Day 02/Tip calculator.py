5cf720fecdef13e8bf88eb5b4138c2cf9643b3b5
Utsab Ghosal <utsabghosal150@gmail.com>
2026-07-11 22:20:26 +0530

    Day 02: Built a Tip Calculator

---
+++ Day 02/Tip calculator.py
@@ -0,0 +1,5 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill?\n$ "))
+tip = int(input("What percentage tip would you like to give?\n"))
+people = int(input("How many people to split the bill?\n"))
+print(f"Each person should pay: $ {round(((bill+((tip/100)*bill))/people), 2)}")
---
+++ Day 02/fString.py
@@ -0,0 +1,3 @@
+#Learnt how to use fStrings
+name=input("What is your name?")
+print(f"Hello {name}")
