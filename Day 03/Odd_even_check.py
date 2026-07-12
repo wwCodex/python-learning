a028385755151203050ae4b4c376d5e99faa3ef8
Utsab Ghosal <utsabghosal150@gmail.com>
2026-07-12 20:21:08 +0530

    Day 03: Learnt if-else and built a Pizaa Bill calculator

---
+++ Day 03/Odd_even_check.py
@@ -0,0 +1,6 @@
+print("Welcome to the Modulo Task")
+num= int(input("Enter the number you want to check.\n"))
+if num % 2 == 0:
+    print ("The number is even.")
+else:
+    print("The number is odd.")
