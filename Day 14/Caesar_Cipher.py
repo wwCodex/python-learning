afc6477e3e783229d1702fdaa809062182ba83e9
Utsab Ghosal <utsabghosal150@gmail.com>
2026-07-31 23:45:33 +0530

    Day 14: Built a Caesar Cipher

--- Day 14/Caesar_Cipher.py
+++ Day 14/Caesar_Cipher.py
@@ -7,7 +7,7 @@ def encrypt(original_text, shift_amount):
             final_text+= alphabet[(alphabet.index(letter) + shift_amount)%26]
 
     print(final_text)
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
 
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
