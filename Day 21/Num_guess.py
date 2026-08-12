b10347f4377d590a05ce88522fc8cee140b3b16a
Utsab Ghosal <utsabghosal150@gmail.com>
2026-08-12 21:55:17 +0530

    Day 21: Built a number guessing game

---
+++ Day 21/Num_guess.py
@@ -0,0 +1,27 @@
+import random
+
+def number_game():
+    number = random.randint(1, 100)
+    attempts = 0
+
+    print(" Welcome to Target Number!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        guess = int(input("\nYour guess: "))
+        attempts += 1
+
+        if guess < number:
+            print("Too low!")
+
+        elif guess > number:
+            print("Too high!")
+
+        else:
+            print(f"\nYou got it!")
+            print(f"The number was {number}.")
+            print(f"Attempts: {attempts}")
+            break
+
+
+number_game()
